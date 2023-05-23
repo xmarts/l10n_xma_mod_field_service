479046bde12bb506a4792e814ccc6a94a1ea44ed
Tu Nombre <you@example.com>
2023-05-22 22:40:54 -0500

    [MOD]enviar data a task desd helpdesk

--- l10n_xma_mod_field_service/models/models.py
+++ l10n_xma_mod_field_service/models/models.py
@@ -20,12 +20,66 @@ _logger = logging.getLogger(__name__)
 class helpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
     
-    @api.onchange('team_id')
-    def create_ticket(self):
-        _logger.info("////////////////XXXXXXXXXXXXXXXXXXXXXX\\\\\\\\\\\\\\\\\\\ %s!"  % ('i'))
-        #if self.team_id.id == 1:
-        self.action_convert_to_task()
+    def action_generate_fsm_task(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Create a Field Service task'),
+            'res_model': 'helpdesk.create.fsm.task',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'use_fsm': True,
+                'default_helpdesk_ticket_id': self.id,
+                'default_user_id': False,
+                'default_partner_id': self.partner_id.id if self.partner_id else False,
+                'default_name': self.name,
+                'default_project_id': self.team_id.fsm_project_id.id,
+                'default_dpi': self.x_studio_dpi,
+                'default_telefono': self.x_studio_telfono,
+                'default_telefono2': self.x_studio_tel_2,
+                'default_monto': self.x_studio_monto,
+                'default_dir_despacho': self.x_studio_direccin_de_despacho,
+                'default_quien_recibe': self.x_studio_nombre_de_quien_recibe,
+                'default_observaciones': self.x_studio_observaciones_generales_para_entrega,
+                'default_fecha_entrega': self.x_studio_fecha_solicitada_de_entrega,
+            }
+        }
     
+    
+class CreateTask(models.TransientModel):
+    _inherit = "helpdesk.create.fsm.task"   
+    
+    dpi = fields.Char()
+    telefono = fields.Char()
+    telefono2 = fields.Char()
+    #forma_pago
+    monto = fields.Float()
+    dir_despacho = fields.Char()
+    quien_recibe = fields.Char()
+    observaciones = fields.Char()
+    #asignado
+    fecha_entrega= fields.Date()
+    
+    
+    def _generate_task_values(self):
+        self.ensure_one()
+        return {
+            'name': self.name,
+            'helpdesk_ticket_id': self.helpdesk_ticket_id.id,
+            'project_id': self.project_id.id,
+            'partner_id': self.partner_id.id,
+            'description': self.helpdesk_ticket_id.description,
+            'x_studio_dpi': self.dpi,
+            'partner_phone': self.telefono,
+            'x_studio_telefono_2': self.telefono2,
+            'x_studio_monto': self.monto,
+            'x_studio_direccin_de_despacho': self.dir_despacho,
+            'x_studio_nombre_de_quien_recibe': self.quien_recibe,
+            'x_studio_observaciones_generales_para_entrega': self.observaciones,
+            'x_studio_fecha_solicitada_de_entrega': self.fecha_entrega,
+        }
+        
 class ProjectTask(models.Model):
     _inherit = "project.task"
     
