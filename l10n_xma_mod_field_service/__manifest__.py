6b914a4af2ed20fea766eb87027afb8646ae2ee9
juancacorpsj2l <jrojas@j2ltechgt.com>
2023-11-04 09:42:52 -0600

    [FIX] dependencias

--- l10n_xma_mod_field_service/__manifest__.py
+++ l10n_xma_mod_field_service/__manifest__.py
@@ -11,9 +11,7 @@
     'website': "",
     'category': 'Accounting',
     'version': '1.0.0',
-
-    'depends': ['base', 'sale','industry_fsm', 'helpdesk','project'],
-
+    'depends': ['base', 'sale','industry_fsm', 'helpdesk','project', 'partner_names', 'xmart_hospital'],
     'data': [
         'security/ir.model.access.csv',
         #'views/res_currency_rate_inherit.xml',
